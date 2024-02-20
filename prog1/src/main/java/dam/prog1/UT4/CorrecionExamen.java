daa4014ebafde96704fde2076740d1dbce481ddb
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-02-20 20:47:17 +0100

    Update UT5 Excersise

--- prog1/src/main/java/dam/prog1/UT4/CorrecionExamen.java
+++ prog1/src/main/java/dam/prog1/UT4/CorrecionExamen.java
@@ -3,8 +3,8 @@ package dam.prog1.UT4;
 import java.util.*;
 
 public class CorrecionExamen {
-	
-	private static final int DIM_COL=10;
+
+	private static final int DIM_COL = 10;
 
 	public static void main(String[] args) {
 		int[][] matriz = ejercicio1();
@@ -16,7 +16,7 @@ public class CorrecionExamen {
 
 	private static int[][] ejercicio1() {
 		System.out.println("Ejercicio 1");
-		
+
 		// Instancia de la clase Random
 		Random rand = new Random();
 		// filasAleatorio= rand.nextInt(5) + 6; rand.nextInt(5,11)
@@ -44,9 +44,9 @@ public class CorrecionExamen {
 	}
 
 	private static int[] ejercicio2(int[][] mat) {
-		
+
 		System.out.println("Ejercicio 2");
-		
+
 		// Declaramos el array que se va a devolver
 		int[] nuevoArray = new int[mat.length];
 
@@ -69,24 +69,21 @@ public class CorrecionExamen {
 	}
 
 	private static int[] ejercicio3(int[][] mat, int[] array) {
-		
+
 		System.out.println("Ejercicio 3");
-		
+
 		int filas = mat.length;
 		int[] subArray = new int[mat[0].length];
 
 		int indiceMayor = 0;
 		int valorMaximo = 0;
-		
+
 		/*
-		 * Correción de alejandra 
-		 * int indiceMayor=0;
-		 * for (int i=1; i<arrSuma.length;i++){
-		 * if(arrSuma[IndiceMayor] < arrSuma[i]){
-		 *  indiceMayor = i;
-		 * Syso la posicion mayor es indiceMayor
+		 * Correción de alejandra int indiceMayor=0; for (int i=1;
+		 * i<arrSuma.length;i++){ if(arrSuma[IndiceMayor] < arrSuma[i]){ indiceMayor =
+		 * i; Syso la posicion mayor es indiceMayor
 		 * 
-		 * int [] arrMayor = mat[indiceMayor]; Esto es algo como el paso por referencia 
+		 * int [] arrMayor = mat[indiceMayor]; Esto es algo como el paso por referencia
 		 */
 
 		// Buscamos la suma maxima
@@ -106,9 +103,40 @@ public class CorrecionExamen {
 		}
 		return subArray;
 	}
-	
-	/*
-	 * En otra clase 
-	 */
-	
+
+	private static int ejercicio4() {
+		System.out.println("Ejercicio 4");
+		Random rand = new Random();
+		int[][] matriz;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime filas");
+		int filas = sc.nextInt();
+		System.out.println("Dime columnas");
+		int columnas = sc.nextInt();
+
+		matriz = new int[filas][columnas];
+		for (int f = 0; f < matriz.length; f++) {
+			for (int c = 0; c < matriz[0].length; c++) {
+				matriz[f][c] = rand.nextInt(5) + 5;
+			}
+
+		}
+		for (int f = 0; f < matriz.length; f++) {
+			for (int c = 0; c < matriz[0].length; c++) {
+			System.out.println(matriz[f][c] + " ");
+			}
+			System.out.println();
+		}
+		
+		for(int c=0;c<columnas;c++) {
+			int aux=matriz[0][0];
+			matriz[0][0]=matriz[0][1];
+			matriz[0][1]=aux;
+		}
+		//Falta mensaje de matriz nueva intercambiada
+
+		return 0;
+
+	}
+
 }
---
+++ prog1/src/main/java/dam/prog1/UT5/ejercicioPersonas/ListaHermanaTorpe.java
@@ -0,0 +1,107 @@
+package dam.prog1.UT5.ejercicioPersonas;
+
+public class ListaHermanaTorpe implements Lista {
+
+	private Nodo primero;
+	// No tiene tamaño
+
+	// No tiene contador de tamaño ni esta refactorizada
+
+	private static void main(String[] args) {
+		Lista lista = new ListaHermanaTorpe();
+		//Tema 3 compatibilidad entre tipos java;
+	}
+
+	public ListaHermanaTorpe(Nodo primero) {
+		this.primero = primero;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return primero == null;
+	}
+
+	@Override
+	public Persona getFirst() {
+		Persona person = null;
+		if(!isEmpty()) {
+			person= primero.getInfo();
+		}
+		return person;
+	}
+
+	@Override
+	public Persona getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertAtBegin(Persona info) {
+		
+		Nodo nuevo = new Nodo(info);
+			
+			if(primero==null) {
+				primero=nuevo;
+		}else {
+			nuevo.setSig(primero);
+			primero=nuevo;
+
+		}
+	}
+
+	@Override
+	public void insertAtEnd(Persona info) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertAtPosition(Persona info, int index) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean containsInfo(Persona info) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Persona getElementAt(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeByInfo(Persona info) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Persona removeAt(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean clearList() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
