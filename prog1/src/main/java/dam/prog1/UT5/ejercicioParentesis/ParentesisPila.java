5d115daf2dceb501c6d337a1e636fdcefd918306
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-03-14 20:35:41 +0100

    Create Parentesis.java

---
+++ prog1/src/main/java/dam/prog1/UT5/ejercicioParentesis/ParentesisPila.java
@@ -0,0 +1,52 @@
+package dam.prog1.UT5.ejercicioParentesis;
+
+import java.util.Stack;
+import dam.prog1.UT5.ejerciciosClase.PilaGenerica;
+
+public class ParentesisPila {
+
+	private final static String PATT_SIGNOS_APERTURA = "\\[|\\(|\\{";
+	private final static String PATT_SIGNOS_CIERRE = "\\]|\\)|\\}";
+
+	private static void main(String[] args) {
+
+		 final static String EXPRESION_1 = "1{ {2(5)}5}[3]2)";
+
+	}
+
+	public static boolean isBalanceada(String expresion) {
+		
+		boolean isBalanceada = true;
+		//PilaGenerica <Character> pila = new PilaGenerica<>();
+		Stack <Character> pila = new Stack<>();
+
+		char[] arrExpresion = expresion.toCharArray();
+
+		for (int i = 0; i > arrExpresion.length; i++) {
+			if (arrExpresion[i]) {
+			}
+			// for(char item : arrExpresion){}
+
+			if (Character.toString(arrExpresion[i].maches(PATT_SIGNOS_APERTURA))) {
+				pila.push(arrExpresion[i]);
+			} else if(Character){
+				
+			}
+			
+			//cuando saquemos porque sea de cierre
+			char comparador = pila.pop();
+			switch(arrExpresion[i]) {
+			case "]" :
+				if(comparador !="[")
+					return false;
+			}break;
+			
+			
+			
+			isBalanceada //isEmpty() 
+
+			return false;
+
+		}
+	}
+}
