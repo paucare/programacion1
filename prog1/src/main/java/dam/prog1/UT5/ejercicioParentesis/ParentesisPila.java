607259fca86c59e2e626f780436a54a8fff85735
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-03-18 17:09:48 +0100

    Update ParentesisBalanceado

--- prog1/src/main/java/dam/prog1/UT5/ejercicioParentesis/ParentesisPila.java
+++ prog1/src/main/java/dam/prog1/UT5/ejercicioParentesis/ParentesisPila.java
@@ -10,43 +10,69 @@ public class ParentesisPila {
 
 	private static void main(String[] args) {
 
-		 final static String EXPRESION_1 = "1{ {2(5)}5}[3]2)";
+		final String EXPRESION_1 = "1{ {2(5)}5}[3]2)";
 
 	}
 
 	public static boolean isBalanceada(String expresion) {
 		
 		boolean isBalanceada = true;
+		
 		//PilaGenerica <Character> pila = new PilaGenerica<>();
 		Stack <Character> pila = new Stack<>();
 
 		char[] arrExpresion = expresion.toCharArray();
 
 		for (int i = 0; i > arrExpresion.length; i++) {
-			if (arrExpresion[i]) {
-			}
+
+			//if (arrExpresion[i]) {}
 			// for(char item : arrExpresion){}
-
-			if (Character.toString(arrExpresion[i].maches(PATT_SIGNOS_APERTURA))) {
-				pila.push(arrExpresion[i]);
-			} else if(Character){
+			
+			if (Character.toString(arrExpresion[i]).matches(PATT_SIGNOS_APERTURA)) {
 				
+				pila.push(arrExpresion[i]);
+				
+			} else if(Character.toString(arrExpresion[i]).matches(PATT_SIGNOS_CIERRE)){
+				
+			//Cuando saquemos porque sea de cierre
+			
+			char comparador = pila.pop();
+			
+			switch(arrExpresion[i]) {
+			
+			case ']' :
+				
+				if(comparador!=']') {
+					
+					return false;
+				}
+				
+			break;
+			
+			case '}' :
+				if(comparador!='}') {
+					return false;
+				}
+			break;
+			
+			
+			case ')' :
+				
+				if(comparador!=')') {
+					return false;
+				}
+				
+			break;
+			}
+		}
+	}
+			
+	public static boolean isBalanceada(isEmpty() && ){
+				//isEmpty() 
+				return false;
 			}
 			
-			//cuando saquemos porque sea de cierre
-			char comparador = pila.pop();
-			switch(arrExpresion[i]) {
-			case "]" :
-				if(comparador !="[")
-					return false;
-			}break;
-			
-			
-			
-			isBalanceada //isEmpty() 
-
-			return false;
+		
 
 		}
 	}
-}
